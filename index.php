d8ba0afe49fcb434db66d9f09c5b5168f6b4b1a6
AdamProchy <prochazka7@spsejecna.cz>
2023-05-15 17:48:13 +0200

    Login + register basic design
    Basic design of login and registration pages

--- index.php
+++ index.php
@@ -11,21 +11,111 @@ include("config.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="register.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Registrace</title>
 </head>
 
 <body>
     <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-        <div class="container">
-            <h1>Registration:</h1>
-            <label for="uname"><b>Username</b></label>
-            <input type="text" name="uname" required>
-            <label for="psw"><b>Password</b></label>
-            <input type="password" name="psw" required>
-            <button type="submit">Register</button>
-            <a href="./login.php">Login</a>
-        </div>
+
+
+        <section class="vh-100 gradient-custom">
+            <div class="container py-5 h-100">
+                <div class="row justify-content-center align-items-center h-100">
+                    <div class="col-12 col-lg-9 col-xl-7">
+                        <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+                            <div class="card-body p-4 p-md-5">
+                                <h1 class="mb-4 pb-2 pb-md-0 mb-md-5">Registrace</h1>
+                                <form>
+
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="firstName" class="form-control form-control-lg" required/>
+                                                <label class="form-label" for="firstName">Jméno</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="lastName" class="form-control form-control-lg" required/>
+                                                <label class="form-label" for="lastName">Přijmení</label>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4 d-flex align-items-center">
+                                            <div class="form-outline datepicker w-100">
+                                                <input type="date" class="form-control form-control-lg" id="birthdayDate" required/>
+                                                <label for="birthdayDate" class="form-label">Datum narození</label>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 mb-4">
+                                            <h6 class="mb-2 pb-1">Pohlaví: </h6>
+
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender" value="option1" checked />
+                                                <label class="form-check-label" for="femaleGender">Žena</label>
+                                            </div>
+
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender" value="option2" />
+                                                <label class="form-check-label" for="maleGender">Muž</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6 mb-4 pb-2">
+
+                                            <div class="form-outline">
+                                                <input type="email" id="emailAddress" class="form-control form-control-lg" />
+                                                <label class="form-label" for="emailAddress">Email</label>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-md-6 mb-4 pb-2">
+                                            <div class="form-outline">
+                                                <input type="password" id="psw" class="form-control form-control-lg" />
+                                                <label class="form-label" for="psw">Heslo</label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-12">
+
+                                            <select class="select form-control-lg">
+                                                <option value="1">rovný</option>
+                                                <option value="2">gej</option>
+                                                <option value="3">lesba</option>
+                                                <option value="4">bi</option>
+                                            </select>
+                                            <label class="form-label select-label">Orientace</label>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="mt-4 pt-2">
+                                        <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
+                                        
+                                    </div>
+                                    <br>
+                                    <a href="./login.php">Přihlásit</a>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
 </html>
