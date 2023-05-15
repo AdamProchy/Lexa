e55e25470ab5296606d4ae4598119935ba38b578
Matyáš Závora <matyaszavora@outlook.com>
2023-05-15 16:56:59 +0200

    DB upload

--- script.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE credentials(
-    ID int primary key identity(1,1),
-    username nvarchar(50) not null,
-    password nvarchar(50) not null
-);
-
-INSERT INTO credentials (username,password)
-VALUES ('admin','admin');
