18b65407ad7ead332332f59a26bd4549741cb3e2
Matyáš Závora <matyaszavora@outlook.com>
2024-03-21 10:36:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 */
 $db_server = "localhost";
 $db_user = "root";
-$db_pass = "Redsteel28!+";
-$db_name = "MojeRande";
+$db_pass = "";
+$db_name = "mojerandedb";
 
 $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
