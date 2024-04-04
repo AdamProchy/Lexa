c3762fd9753ec1ecdf5a75b1f6ce6034935f7e0b
Prochyxd <Adamoso13MC@gmail.com>
2024-04-04 11:20:35 +0200

    light dark theme on home page

--- home.php
+++ home.php
@@ -45,7 +45,7 @@ while ($row = mysqli_fetch_assoc($result)) {
         <title>🖤 Moje Rande 🧡</title>
     </head>
 
-    <body class="d-flex flex-column min-vh-100">
+    <body class="d-flex flex-column min-vh-100" id="body">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark nav-underline">
         <div class="container-fluid">
             <a class="navbar-brand" href="./index.php"><img src="./img/logo.png" width="200px" height="50px"></a>
@@ -55,7 +55,7 @@ while ($row = mysqli_fetch_assoc($result)) {
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" href="./home.php">Domu</a>
+                        <a class="nav-link active" href="./home.php" style="color: #ff9900;">Domu</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./date.php">Chci rande!</a>
@@ -71,14 +71,18 @@ while ($row = mysqli_fetch_assoc($result)) {
                     <li class="nav-item">
                         <button id="switch" class="btn nav-link" onclick="toggleTheme()">Switch</button>
                     <li class="nav-item">
-                        <p class="navbar-text text-white">Přihlášen: </p>
+                        <a class="nav-link" href="./shop.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16">
+  <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518z"/>
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+  <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11m0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12"/>
+</svg></a>
                     </li>
                     <li class="nav-item me-2">
-                        <a class="nav-link text-warning"
+                        <a class="nav-link" style="color: #ff9900;"
                            href="./settings.php"><?php echo $firstName . " " . $lastName ?></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-danger" href="./logout.php">Odhlásit se</a>
+                        <a class="nav-link" style="color: red;" href="./logout.php">Odhlásit se</a>
                     </li>
                 </ul>
             </div>
@@ -167,10 +171,10 @@ while ($row = mysqli_fetch_assoc($result)) {
         <div class="container">
             <?php
             if (empty($dates)) {
-                echo "<h2 class='text-center text-dark'>Nemáte žádné domluvené schůzky.</h2>";
+                echo "<h2 class='text-center'>Nemáte žádné domluvené schůzky.</h2>";
             } else {
                 ?>
-                <h2 class="text-center text-dark">Domluvené schůzky:</h2>
+                <h2 class="text-center">Domluvené schůzky:</h2>
                 <br>
                 <div class="row g-4">
 
@@ -232,10 +236,11 @@ while ($row = mysqli_fetch_assoc($result)) {
             <?php } ?>
         </div>
     </section>
-    <footer class="p-5 bg-dark text-white text-center position-relative mt-auto">
+    <footer class="bg-dark text-white text-center position-relative mt-auto">
         <div class="container">
-            <p class="lead">Copyright &copy; PROCHY</p>
-            <a href="#" class="position-absolute bottom-0 end-0 p-5"><i class="bi-arrow-up-circle h1"></i></a>
+            <p class="lead">Copyright &copy; PROCHY | SPŠE Ječná</p>
+            <a href="#" class="position-absolute bottom-0 end-0 p-5"><i class="bi-arrow-up-circle h1"
+                                                                        style="color: #ff9900;"></i></a>
         </div>
     </footer>
 
--- scripts/night-theme.js
+++ scripts/night-theme.js
@@ -8,12 +8,16 @@ function setTheme(themeName) {
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
-        document.documentElement.setAttribute('data-bs-theme','light')
-        document.getElementById('switch').innerHTML = '🌞';
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+        document.getElementById('switch').innerHTML = '<i class="bi bi-brightness-high"></i>';
+        document.body.classList.remove('bg-dark');
+        document.body.classList.add('bg-light');
     } else {
         setTheme('theme-dark');
-        document.documentElement.setAttribute('data-bs-theme','dark')
-        document.getElementById('switch').innerHTML = '🌙';
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        document.getElementById('switch').innerHTML = '<i class="bi bi-moon"></i>';
+        document.body.classList.remove('bg-light');
+        document.body.classList.add('bg-dark');
     }
 }
 
@@ -22,10 +26,14 @@ function toggleTheme() {
 (function () {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-dark');
-        document.getElementById('switch').innerHTML = '🌙';
+        document.getElementById('switch').innerHTML = '<i class="bi bi-moon"></i>';
+        document.body.classList.remove('bg-light');
+        document.body.classList.add('bg-dark');
     } else {
         setTheme('theme-light');
-        document.getElementById('switch').innerHTML = '🌞';
+        document.getElementById('switch').innerHTML = '<i class="bi bi-brightness-high"></i>';
+        document.body.classList.remove('bg-dark');
+        document.body.classList.add('bg-light');
 
     }
 })();
