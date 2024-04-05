8876bb0f3c8405e6f017f80f1db9654cd64bfdcb
Prochyxd <Adamoso13MC@gmail.com>
2024-04-05 13:40:10 +0200

    mode switch on settings

--- settings.php
+++ settings.php
@@ -4,6 +4,7 @@ include('utils.php');
 $email = $_SESSION["email"];
 $firstName = $_SESSION["firstName"];
 $lastName = $_SESSION["lastName"];
+$Id = $_SESSION["ID"];
 $isImage = true;
 $updated = false;
 $emailError = false;
@@ -78,19 +79,17 @@ $profilePicture = $_SESSION["profilePicture"];
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-              crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <link rel="stylesheet" href="styles/settings.css">
         <link rel="icon" type="image/x-icon" href="protected/img/favicon.ico">
         <title>🖤 Chci rande! 🧡</title>
     </head>
 
-    <body class="d-flex flex-column min-vh-100">
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <body class="d-flex flex-column min-vh-100" id="body">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark nav-underline">
         <div class="container-fluid">
-            <a class="navbar-brand" href="./index.php"><img src="protected/img/logo.png" width="200px"
+            <a class="navbar-brand" href="./index.php"><img src="./protected/img/logo.png" width="200px"
                                                             height="50px"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText">
                 <span class="navbar-toggler-icon"></span>
@@ -111,15 +110,32 @@ $profilePicture = $_SESSION["profilePicture"];
                     </li>
                 </ul>
                 <ul class="navbar-nav mt-2 mb-2 mb-lg-0">
+
                     <li class="nav-item">
-                        <p class="navbar-text text-white">Přihlášen: </p>
-                    </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link text-warning"
-                           href="./settings.php"><?php echo $firstName . " " . $lastName ?></a>
-                    </li>
+                        <button id="switch" class="btn nav-link" onclick="toggleTheme()"></button>
                     <li class="nav-item">
-                        <a class="nav-link text-danger" href="./logout.php">Odhlásit se</a>
+                        <a class="nav-link" href="./shop.php">
+                            999999
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-coin" viewBox="0 0 16 16">
+                                <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518z"/>
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11m0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <?php if ($Id == 1) { ?>
+                        <li class="nav-item me-2">
+                            <a class="nav-link" style="color: #ff9900;" href="./admin.php">Admin panel</a>
+                        </li>
+                    <?php } else { ?>
+                        <li class="nav-item me-2">
+                            <a class="nav-link active fw-bold" style="color: #ff9900;"
+                               href="./settings.php"><?php echo $firstName . " " . $lastName ?></a>
+                        </li>
+                    <?php } ?>
+                    <li class="nav-item">
+                        <a class="nav-link" style="color: red;" href="./logout.php">Odhlásit se</a>
                     </li>
                 </ul>
             </div>
@@ -183,12 +199,12 @@ $profilePicture = $_SESSION["profilePicture"];
                                 <h6 class="user-email"><?php echo $email ?></h6>
                             </div>
                             <div class="about">
-                                <h5 class="mb-2 text-light">O mně:</h5>
+                                <h5 class="mb-2">O mně:</h5>
                                 <p><?php echo $aboutMe;
                                     $isImage = true; ?></p>
                             </div>
                             <div class="age">
-                                <h5 class="mb-2 text-light">Věk:</h5>
+                                <h5 class="mb-2">Věk:</h5>
                                 <p><?php echo $diff->format('%y'); ?></p>
                             </div>
                         </div>
@@ -201,7 +217,7 @@ $profilePicture = $_SESSION["profilePicture"];
                         <form method="POST" action="" enctype="multipart/form-data">
                             <div class="row gutters">
                                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                    <h4 class="mb-3 text-light">Změnit osobní informace</h6>
+                                    <h4 class="mb-3">Změnit osobní informace</h6>
                                 </div>
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                     <div class="form-group">
@@ -221,7 +237,7 @@ $profilePicture = $_SESSION["profilePicture"];
                             <hr>
                             <div class="row gutters mt-3">
                                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                    <h4 class="mb-3 text-light">Změnit heslo</h6>
+                                    <h4 class="mb-3">Změnit heslo</h6>
                                 </div>
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                     <div class="form-group">
@@ -241,7 +257,7 @@ $profilePicture = $_SESSION["profilePicture"];
                             <hr>
                             <div class="row gutters mt-3">
                                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                    <h4 class="mb-3 text-light">Změnit profilový obrázek</h6>
+                                    <h4 class="mb-3">Změnit profilový obrázek</h6>
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" type="file" name="profilePicture"/>
@@ -268,6 +284,7 @@ $profilePicture = $_SESSION["profilePicture"];
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
             crossorigin="anonymous"></script>
+            <script src="./scripts/night-theme.js"></script>
     </body>
 
     </html>
